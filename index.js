c61d0869b05a7e107478b73e67f653ca0b3285c1
39oMx <king39om07@gmail.com>
2026-07-28 18:00:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function saveConfig(data) {
 const fontPath = path.join(__dirname, 'src', 'templates', 'BodoniFLF.ttf');
 GlobalFonts.registerFromPath(fontPath, 'Bodoni FLF');
 
-// ⬇️ === [ أضف أيديهات الرتب والأسماء المخصصة هنا بالترتيب ] === ⬇️1416304413844439180
+// ⬇️ === [ أضف أيديهات الرتب والأسماء المخصصة هنا بالترتيب ] === ⬇️1431452982008090705
 const CUSTOM_ROLES = [
     { id: '1150626780550533141', displayName: 'OWNER' },
     { id: '1212529351396954162', displayName: 'CEO' },
@@ -71,7 +71,8 @@ const CUSTOM_ROLES = [
     { id: '1515054401881112606', displayName: 'QUALITY CONTROL' },
     { id: '1495722899204472872', displayName: 'RG LEADER' },
     { id: '1524770799888633886', displayName: 'AMBASSADOR' },
-    { id: '1416430043655176262', displayName: '7R MEMBER' }    
+    { id: '1416430043655176262', displayName: '7R MEMBER' },
+    { id: '1431452982008090705', displayName: '7R MEMBER' }
 ];
 
 const rankConfigurations = {
